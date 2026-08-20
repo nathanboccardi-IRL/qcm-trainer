e8cfb3969eac2af33e90a4e6d5009fb302f451a7
Nathan <nathanboccardi@gmail.com>
2026-08-20 09:42:18 +0100

    Trigger auth deadlock fix

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='qcm-trainer-v4';
+const CACHE='qcm-trainer-v5';
+// auth-fix trigger
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
